ca1050b9285461fd983cb98e30013b87a34b8d48
walecloud <walecloud@gmail.com>
2018-05-18 09:22:17 +0100

    added tests for email validation and empty password

--- test/user/model.js
+++ test/user/model.js
@@ -40,6 +40,25 @@ describe('<Unit Test>', function() {
             });
         });
 
+            it('should be able to show an error when trying to save without a valid email', function(done) {
+                var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                user.email = re.test(String(user.email).toLowerCase());
+                return user.save(function(err) {
+                    should.exist(err);
+                    done();
+                });
+            });
+        });
+
+            it('should be able to show an error when trying to save without password', function(done) {
+                user.password = '';
+                return user.save(function(err) {
+                    should.exist(err);
+                    done();
+                });
+            });
+        });
+
         after(function(done) {
             done();
         });
