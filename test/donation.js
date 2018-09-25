6a5b33277886fb8f77b8116ccff17b34b2a6585b
Rafael Wanjiku <phaelgi@gmail.com>
2018-09-25 17:54:05 +0300

    add donation test implemented

--- test/donation.js
+++ test/donation.js
@@ -19,7 +19,10 @@ describe('Donations', function() {
             },
             user:{
                   _id: "1",
-                  premium: "1"
+                  premium: "1",
+                  donations:{
+                    crowdrise_donation_id: "142124"
+                  }
             }
             };
 
@@ -28,15 +31,36 @@ describe('Donations', function() {
             done();
         });
 
-           describe('Test1', function() {
-            it('should test that user and body are not empty', function(done) {
+           describe('Add Donations', function() {
+           
+            it('should return that user and body are empty', function(done) {
                
                 req.should.have.property('body');
                 req.should.have.property('user');
+                req.user.should.have.property('_id');
+
+                it('should compare icd of person donating and the donation id', function() {
+               
+                    req.body.should.have.property('amount');
+                    req.body.should.have.property('crowdrise_donation_id');
+                    req.body.should.have.property('donor_name');
+
+                    var duplicate=false;
+                    for(i=0;i<req.user.should.have.length(2);i++){
+                       it('should equal to',function(){
+                        req.user.Donations[i].crowdrise_donation_id.should.be.eq(req.body.crowdrise_donation_id);
+                        duplicate=true;
+                       }); 
+                    }
+                
+                });
                 
                     done();
                 });
+           
+             
             });
+
 
           
         });       
